87b12ba36d87768876f179228268ac5f98b45e85
Sammy <163420512+sammymices@users.noreply.github.com>
2024-10-20 16:57:12 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login - tudulis</title>
+    <link rel="stylesheet" type="text/css" href="loginstyle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="path-to-sweetalert/sweetalert2.min.css">
+</head>
+<body style="background-color: #374151;">
+
+</body>
+</html>
