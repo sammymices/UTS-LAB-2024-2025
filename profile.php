2e09979659ce0a6278b0698bc7e2e5a8f68c8caf
GodVeillgard <165175903+GodVeillgard@users.noreply.github.com>
2024-10-25 22:34:22 +0700

    -

--- profile.php
+++ profile.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION['user_id'])) {
 
 $user_id = $_SESSION['user_id'];
 
-// Ambil data user
+
 $stmt = $pdo->prepare("SELECT * FROM users WHERE id = ?");
 $stmt->execute([$user_id]);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
